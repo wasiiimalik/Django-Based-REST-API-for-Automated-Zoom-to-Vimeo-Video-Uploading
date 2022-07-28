c838360fe5668036d7672597df896913c3d59950
wasim malik <officialwasimmajidmalik@gmail.com>
2022-07-28 18:20:40 +0530

    updated project

--- ZoomVimeoProjectGithub/vimeoproject/vimeoproject/settings.py
+++ ZoomVimeoProjectGithub/vimeoproject/vimeoproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure---o8x2r(x^57@8bllfrh^ut_(y%kh2l&48snidkk%)x@_6w4p%'
+SECRET_KEY = 'django secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
